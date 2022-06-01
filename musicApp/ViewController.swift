7174eb4fd0a046311e5f2567148bd536c465645f
Alvie <alvieforwork@gmail.com>
2022-06-01 13:38:44 +0800

    Initial Commit

---
+++ musicApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  musicApp
+//
+//  Created by worker on 2022/6/1.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
